5dbcac5cd0b6d36aee2e5d336eba54847a094d56
Hugo Alejandro Gomez Herrera <a01640856@itesm.mx>
2022-06-30 17:09:26 -0700

    Working on profile view

---
+++ Instagram/View Controllers/ProfilePageViewController.m
@@ -0,0 +1,31 @@
+//
+//  ProfilePageViewController.m
+//  Instagram
+//
+//  Created by Hugo Gomez Herrera on 6/30/22.
+//
+
+#import "ProfilePageViewController.h"
+
+@interface ProfilePageViewController ()
+
+@end
+
+@implementation ProfilePageViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
